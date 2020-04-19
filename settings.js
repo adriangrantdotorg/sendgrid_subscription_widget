05f72d581157ae73d9d1656badf950627dd49c9d
Adrian Grant <Adrian@AdrianGrant.org>
2020-04-19 17:29:12 -0400

    ready to test initial setup

--- settings.js
+++ settings.js
@@ -1,12 +1,13 @@
 // Change the url to the domain of your app
-exports.url = 'http://localhost:3090';
+// exports.url = 'http://localhost:3090';
+exports.url = 'https://playthelobby.herokuapp.com/';
 
-exports.senderEmail = "sender@example.com";
-exports.senderName = "Sender Name";
+exports.senderEmail = "donotreply@playthelobby.com";
+exports.senderName = "Naga";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = null;
+exports.listId = 14cbc9ba-79bf-4a2b-97e7-858321fdb5ec;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
@@ -14,4 +15,4 @@ exports.templateId = null;
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
-exports.notificationEmail = "admin@example.com";
+exports.notificationEmail = "Adrian@AdrianGrant.org";
