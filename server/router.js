c3a684cd1cf56a5cca713fc0bd84557cfe9e9866
Adrian Grant <Adrian@AdrianGrant.org>
2020-04-23 15:27:10 -0400

    revert back to default - still not working

--- server/router.js
+++ server/router.js
@@ -3,8 +3,7 @@ const ContactList = require('./controllers/contact_list_controller');
 
 module.exports = function(app) {
 	app.get('/', function(req, res) { res.sendFile(path.join(__dirname, '/static/index.html')) });
-	// app.get('/success', function(req, res) { res.sendFile(path.join(__dirname, '/static/success.html')) });
+	app.get('/success', function(req, res) { res.sendFile(path.join(__dirname, '/static/success.html')) });
 	app.post('/confirmEmail', ContactList.sendConfirmation);
-	app.post('/success', ContactList.addUser);
 	app.post('/signup', ContactList.addUser);
 }
